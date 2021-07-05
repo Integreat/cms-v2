d9cc7d5e2431d2f1aa6d01b91a13e8c7e04a4547
David Venhoff <venhoff@integreat-app.de>
2021-07-05 17:33:27 +0200

    implement completion ui

--- src/frontend/js/tinymce-plugins/custom_link_input/plugin.js
+++ src/frontend/js/tinymce-plugins/custom_link_input/plugin.js
@@ -1,41 +1,63 @@
-// import { default as tinymce } from "tinymce";
-
 (function () {
     'use strict'
 
+    const tinymceConfig = document.getElementById("tinymce-config-options");
+
+    async function getCompletions(query) {
+        return [];
+    }
 
     tinymce.PluginManager.add('custom_link_input', function (editor, url) {
-        
+
         const open_dialog = function () {
-            let old_link_text = "";
+            let old_search_text = "";
             let old_link_url = "";
-            
-            return editor.windowManager.open({
-                title: "Open Link (Translate)",
+
+            // Stores the current request index, so that outdated requests get ignored
+            let ajax_request_id = 0;
+
+            let completion_items = [];
+            const dialog_config = {
+                title: tinymceConfig.getAttribute("data-link-dialog-title-text"),
                 body: {
                     type: "panel",
                     items: [
                         {
                             type: "input",
                             name: "url",
-                            label: "URL (Translate)"
+                            label: tinymceConfig.getAttribute("data-link-dialog-url-text")
                         },
                         {
                             type: "input",
                             name: "text",
-                            label: "Text to show (Translate)",
+                            label: tinymceConfig.getAttribute("data-link-dialog-text-text")
+                        },
+                        {
+                            type: "label",
+                            label: tinymceConfig.getAttribute("data-link-dialog-internal_link-text"),
+                            items: [
+                                {
+                                    type: "input",
+                                    name: "search",
+                                },
+                                {
+                                    type: "selectbox",
+                                    name: "completions",
+                                    items: completion_items
+                                }
+                            ]
                         }
                     ]
                 },
                 buttons: [
                     {
                         type: "cancel",
-                        text: "Close (Translate)"
+                        text: tinymceConfig.getAttribute("data-dialog-cancel-text")
                     },
                     {
                         type: "submit",
                         name: "submit",
-                        text: "Insert (Translate)",
+                        text: tinymceConfig.getAttribute("data-dialog-submit-text"),
                         primary: true,
                         disabled: true
                     },
@@ -50,25 +72,25 @@
                     let url = data.url;
                     const text = data.text || url;
 
+                    // Otherwise tinymce will append the url to the current page url
                     if (url.toLowerCase().startsWith("www.")) {
                         url = "https://" + url;
                     }
-                    
+
                     if (data.url.trim() == "") {
                         return;
                     }
                     editor.insertContent(`<a href=${url}>${text}</a>`);
                     api.close();
                 },
-                onChange: function(api) {
+                onChange: function (api) {
                     let data = api.getData();
-
                     // Automatically update the text button to the url by default
                     if (data.url != data.text && old_link_url == data.text) {
-                        api.setData({text: data.url});
-                    } 
+                        api.setData({ text: data.url });
+                    }
                     old_link_url = data.url;
-                    
+
                     // Disable the submit button if either of url and text are empty
                     data = api.getData();
                     if (data.url.trim() && data.text.trim()) {
@@ -76,12 +98,27 @@
                     } else {
                         api.disable("submit");
                     }
+
+                    if (data.search != old_search_text) {
+                        getCompletions(data.search).then((completions) => {
+                            console.log("Got: ", completions);
+                            completion_items.push({"text": "text", "value": "value"});
+
+                            // It seems like there is no better way to update the completion list 
+                            api.redial(dialog_config);
+                            api.setData(data);
+                            api.focus("search");
+                        });
+                    }
+                    old_search_text = data.search;
                 }
-            });
+            };
+
+            return editor.windowManager.open(dialog_config);
         };
 
         editor.ui.registry.addMenuItem('add_link', {
-            text: "Add Link (Translate)",
+            text: tinymceConfig.getAttribute("data-link-add-text"),
             onAction: open_dialog,
         });
 
