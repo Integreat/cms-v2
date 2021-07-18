abea02fbe14faff4008c5507293c144ab1b17842
David Venhoff <venhoff@integreat-app.de>
2021-07-18 11:41:50 +0200

    Prefix static urls in _tinymce_config.html with the base url

--- src/cms/templatetags/settings_tags.py
+++ src/cms/templatetags/settings_tags.py
@@ -3,7 +3,7 @@ This contains tags for accessing settings
 """
 from django import template
 
-from backend.settings import WEBAPP_URL
+from backend.settings import BASE_URL, WEBAPP_URL
 
 
 register = template.Library()
@@ -18,3 +18,14 @@ def get_webapp_url():
     :rtype: str
     """
     return WEBAPP_URL
+
+
+@register.simple_tag
+def get_base_url():
+    """
+    This tag returns the ``BASE_URL``
+
+    :return: The base url of the current web application
+    :rtype: str
+    """
+    return BASE_URL
